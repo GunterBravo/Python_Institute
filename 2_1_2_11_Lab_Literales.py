ccc57020766efb6ab8843442088b82af312e1f23
Gunter <gunterbravoc@gmail.com>
2021-01-19 18:42:39 -0500

    2.1.2.11 LABORATORIO: Literales de Python - cadenas

---
+++ 2_1_2_11_Lab_Literales.py
@@ -0,0 +1,9 @@
+"""Escenario
+Escribe una sola línea de código, utilizando la función print(), así como los caracteres de nueva línea y escape, para obtener la salida esperada de tres líneas."""
+
+''' Salida Esperada
+"Estoy"
+""aprendiendo""
+"""Python"""    '''
+print()
+print(' "Estoy"\n""aprendiendo""\n"""Python""" ')
