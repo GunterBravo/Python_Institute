17ccb02b3633e1ba8422f1679165ded2eb603028
Gunter <gunterbravoc@gmail.com>
2021-01-18 23:49:32 -0500

    primer commit

---
+++ 2_1_1_19_Lab_Funcion_Print.py
@@ -0,0 +1,8 @@
+print("Fundamentos","Programación","en")
+print("Python")
+#El resultado esperado es:
+#Fundamentos***Programación***en...Python
+print()
+print("Desde aquí es la solución")
+print("Fundamentos","Programación", "en", sep="***",  end="...")
+print("Python")
