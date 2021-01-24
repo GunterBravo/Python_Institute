cf5f327b01724c6174dbd1db710192a5a708d185
Gunter <gunterbravoc@gmail.com>
2021-01-24 10:03:08 -0500

    2_1_5_2_Lab_Comentarios Resuelto

---
+++ 2_1_5_2_Lab_Comentarios.py
@@ -0,0 +1,10 @@
+print("este programa calcula los segundos en cierto número de horas determinadas")
+# este programa fue escrito hace dos días
+
+a = int(input("ingresa la cantidad de horas: ")) # numero de horas
+segundos = 3600 # número de segundos en una hora
+
+print("Segundos en horas: ", a * segundos) # se imprime el numero de segundos en determinado numero de horas
+
+print ("Adiós")
+print("este el es fin del programa que calcula el numero de segundos en " + str(a) + " Horas")
